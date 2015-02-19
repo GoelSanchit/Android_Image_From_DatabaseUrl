0b9e035040d745ce02f703297642ec300a6ffd70
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-02-19 23:33:43 +0530

    Create imagefetch.php

---
+++ app/src/imagefetch.php
@@ -0,0 +1,20 @@
+<?php
+ 
+$objConnect = mysql_connect("Host_name","Host_Username","password");
+$objDB = mysql_select_db("database_name");
+$strSQL = "SELECT * FROM image WHERE 1 ";
+$objQuery = mysql_query($strSQL);
+$intNumField = mysql_num_fields($objQuery);
+$resultArray = array();
+while($obResult = mysql_fetch_array($objQuery))
+{
+$arrCol = array();
+for($i=0;$i<$intNumField;$i++)
+{
+$arrCol[mysql_field_name($objQuery,$i)] = $obResult[$i];
+}
+array_push($resultArray,$arrCol);
+}
+mysql_close($objConnect);
+echo json_encode($resultArray);
+?>
